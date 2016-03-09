37b9b67055d951d4de5186f54f5c0229e0cc9ca5
Lorna Jane Mitchell <lorna@lornajane.net>
2016-03-09 21:41:57 +0000

    Populate components table with some entries

--- db/migrations/20160111202556_tickets_table.php
+++ db/migrations/20160111202556_tickets_table.php
@@ -4,32 +4,17 @@ use Phinx\Migration\AbstractMigration;
 
 class TicketsTable extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
-    public function change()
+    public function up()
     {
         $components_table = $this->table('components');
         $components_table->addColumn('component', 'string')
             ->create();
+
+        $components_table->insert([
+            ["component" => "website"], 
+            ["component" => "mobile app"]
+        ]);
+        $components_table->saveData();
 
         $tickets_table = $this->table('tickets');
         $tickets_table->addColumn('title', 'string')
@@ -38,5 +23,10 @@ class TicketsTable extends AbstractMigration
             ->addForeignKey('component_id', 'components', 'id')
             ->create();
 
+    }
+
+    public function down() {
+        $this->dropTable('tickets');
+        $this->dropTable('components');
     }
 }
